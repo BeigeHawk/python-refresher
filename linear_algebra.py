0b5b52e392e46f86195e1aca3f597de554484e54
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-08 19:29:37 -0400

    Linalg Problem 4

--- linear_algebra.py
+++ linear_algebra.py
@@ -32,3 +32,14 @@ dot_product = np.dot(a, b)
 print("Problem 3")
 print(f"Dot product of vectors: {dot_product}")
 print("")
+
+# Problem 4
+A2 = np.array([[1,2,3],[4,5,6]])
+B2 = np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])
+
+matrix_product = np.dot(A2, B2)
+
+print("Problem 4")
+print(f"Product of matrices: {matrix_product}")
+print("")
+
