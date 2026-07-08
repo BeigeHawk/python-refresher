296876b57e4ab4873dcd4a79bb641ce04e79862c
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-08 19:25:07 -0400

    Lingalg Problem 3

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# Problem 1
 a = np.array([1,2,3])
 b = np.array([4,5,6])
 
@@ -12,6 +12,7 @@ print(f"Sum of vectors: {vector_sum}")
 print(f"Difference of vectors: {vector_dif}")
 print("")
 
+# Problem 2
 A = np.array([[1,2],[3,4]])
 B = np.array([[5,6],[7,8]])
 
@@ -21,4 +22,13 @@ matrix_dif = A - B
 print("Problem 2")
 print(f"Sum of matrices: {matrix_sum}")
 print(f"Difference of vectors: {matrix_dif}")
+print("")
+
+# Problem 3
+# a and b are already properly defined from Problem 1
+
+dot_product = np.dot(a, b)
+
+print("Problem 3")
+print(f"Dot product of vectors: {dot_product}")
 print("")
