af751649940a6c0e2af91d1b031891f4c9fdf79e
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-08 19:10:43 -0400

    Linalg Problem 1

--- linear_algebra.py
+++ linear_algebra.py
@@ -3,3 +3,11 @@ import numpy as np
 
 a = np.array[1,2,3]
 b = np.array[4,5,6]
+
+vector_sum = a + b 
+vector_dif = a - b
+
+print("Problem 1")
+print(f"Sum of vectors: {vector_sum}")
+print(f"Difference of vectors: {vector_dif}")
+
