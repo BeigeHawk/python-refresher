c66dcbe8010dbc9b079475a261db0841cef541a1
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-08 21:04:50 -0400

    Completing bank account program

--- bank_account.py
+++ bank_account.py
@@ -4,19 +4,26 @@ import numpy as np
 class BankAccount:
     def __init__(self, name: str, account_number: int, balance: float):
         if self.balance < 0:
-            print("Balance cannot be negative")
+            raise ValueError("Balance cannot be negative")
         self.name = name
         self.account_number = account_number
         self.balance = balance
 
-    def transaction(self, amount):
-        self.balance += amount
-    
     def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount to withdraw must be a positive number")
+        if amount > self.balance:
+            raise ValueError("You have insufficient funds")
         self.balance -= amount
+        return self.balance
 
     def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount to deposit must be a positive number")
         self.balance += amount
+        return self.balance
 
-    def print(self, balance):
-        print(f"Hello {self.name}, your balance is {self.balance}.")
+    def print_balance(self):
+        balance_message = f"Hello {self.name}, the balance for account {account_number} is {self.balance}."
+        print(balance_message)
+        return balance_message
---
+++ test_bank_account.py
@@ -0,0 +1,8 @@
+import unittest
+import bank_account
+import numpy as np
+
+
+class TestAccount(unittest.TestCase):
+    def test_transaction(self):
+        self.assertEqual()
