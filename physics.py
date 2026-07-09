5cc35c4de119678a1cd52618a0c8b84a110d1d15
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-09 10:29:13 -0400

    Physics Problem 2

--- physics.py
+++ physics.py
@@ -8,3 +8,12 @@ def calculate_buoyancy(self):
     volume = self.volume
     buoyant_force = fluid_density * volume * g
     return buoyant_force
+
+# Problem 2
+def calculate_pressure(self):
+    g = 9.81
+    fluid_density = 1000
+    depth = self.depth
+    pressure = fluid_density * g * depth
+    return pressure
+
