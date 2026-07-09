31ee13bdaa943360a8cb5b9041a0fa27a4f22ab5
Noah Thrasher <nthrasher92973@gmail.com>
2026-07-09 10:32:49 -0400

    Physics Problem 3

--- physics.py
+++ physics.py
@@ -10,6 +10,20 @@ def calculate_buoyancy(self):
     return buoyant_force
 
 # Problem 2
+def will_it_float(self):
+    buoyant_force = self.calculate_buoyancy()
+    V = self.volume
+    mass = self.mass
+    weight = mass * 9.81
+    if buoyant_force > weight:
+        return True
+    else:
+        if buoyant_force == weight:
+            return "The object is neutrally buoyant"
+        else:
+            return False
+
+# Problem 3
 def calculate_pressure(self):
     g = 9.81
     fluid_density = 1000
@@ -17,3 +31,4 @@ def calculate_pressure(self):
     pressure = fluid_density * g * depth
     return pressure
 
+    
